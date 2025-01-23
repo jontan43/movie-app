3ee6aa05b9ed06b8a55d53d6bdf5fa3392dc5648
jontan43 <jonathantan43@gmail.com>
2025-01-23 21:20:07 +0800

    Testing dev branch

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <MovieCard movie={{title:"Avengers", release_date:"23 Jan 2025", url:"https://m.media-amazon.com/images/I/719SFBdxRtL._AC_UF894,1000_QL80_.jpg"}}/>
-      <MovieCard movie={{title:"Jack", release_date:"23 Jan 2025"}}/>
+      <MovieCard movie={{title:"Batman",url:"https://m.media-amazon.com/images/I/81IfoBox2TL.jpg" ,release_date:"23 Jan 2025"}}/>
     </>
   )
 }
